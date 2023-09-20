ec29a9e798dca55d15e58fa7f6e56f591dc50709
kokarevishakha <vishakhakokare07@gmail.com>
2023-09-20 13:07:37 +0530

    Variable Assignment

---
+++ imges/01_variableAssign.js
@@ -0,0 +1,36 @@
+console.log("********************STEP-01*******************************");
+
+var myFullName;
+console.log(myFullName);
+myFullName = "Vishakha Kokare";
+console.log(myFullName);
+console.log("My Full Name:-", "Vishakha Shankar Kokare");
+
+console.log("********************STEP-02*******************************");
+
+
+var myLove="I Love Only JS❤️...";
+console.log(myLove);
+
+console.log("********************STEP-03*******************************");
+
+
+var firstName;
+console.log("First Name:" ,"Vishakha");
+
+var lastName;
+console.log("Last Name:" ,"Kokare");
+
+var age;
+console.log("Age:" ,"23");
+
+var Address;
+console.log("Address:" ,"Baramati");
+
+var pincode;
+console.log("Pin-Code:" ,"413110");
+
+var MaritalStatus;
+console.log("MaritalStatus:" ,"NO");
+
+console.log("#################**END**##########################");
