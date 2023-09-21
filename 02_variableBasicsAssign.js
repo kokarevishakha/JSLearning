43c9ff459cd686771380827f99bf2a8db350bbf3
kokarevishakha <vishakhakokare07@gmail.com>
2023-09-21 13:59:37 +0530

    Create a variable using different data type
    Create a variable using different data type

---
+++ 02_variableBasicsAssign.js
@@ -0,0 +1,41 @@
+console.log("***************** ASSIGNMENT 01 **********************");
+
+console.log("\n***************** 3 Different Data types **********************");
+
+var numberVariable = 123;
+var numVariable = 100;
+var numVar = 200;
+
+console.log("\nNumberVariable :", numberVariable, "   type :", typeof numberVariable);
+console.log("NumberVariable :", numVariable, "     type :", typeof numVariable);
+console.log("NumberVariable :", numVar, "         type :", typeof numVar);
+
+console.log("\n***************** STRING **********************");
+
+var stringVariable = "Hello, world😊!";
+var strVariable = "Hello, India❤️!!";
+var strVar = "Hello, Pune😍!";
+
+console.log("\nStringVariable :", stringVariable, "    type of Data:", typeof stringVariable);
+console.log("StringVariable :", strVariable, "    type of Data:", typeof strVariable);
+console.log("StringVariable :", strVar, "      type of Data:", typeof strVar);
+
+console.log("\n***************** BOOLEAN **********************");
+
+var isTrue = true;
+var isFalse = false;
+var isSunny = true;
+
+console.log("\nBooleanVariable :", isTrue, "     type of Data :", typeof isTrue);
+console.log("BooleanVariable :",isFalse, "       type of Data :", typeof isFalse);
+console.log("BooleanVariable :",isSunny, "        type of Data :", typeof isSunny);
+
+console.log("\n***************** Difference Between Undefined and Null **********************");
+
+var myVariable = null;
+console.log("\nNULL:", myVariable, "    type of Data:", typeof myVariable);
+
+console.log("\n***************** UNDEFINED **********************");
+
+var myVar;
+console.log("\nUndefined:", myVar, "   type of Data:", typeof myVar);
