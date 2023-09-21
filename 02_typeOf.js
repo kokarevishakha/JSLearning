8a1d35c53283aaf437d5a98d197b3bfc0e246f3f
kokarevishakha <vishakhakokare07@gmail.com>
2023-09-21 14:48:56 +0530

    different type of variable.
    different type of variable.

---
+++ 02_typeOf.js
@@ -0,0 +1,58 @@
+console.log(
+  "\n***************** ASSIGNMENT 02 (TYPE OF JS) **********************"
+);
+console.log("\n ***************** Task -  01 **********************");
+console.log(
+  "\n ***************** Before Initialization **********************"
+);
+
+var bankName;
+console.log("Bank Name:", bankName, "       Type Of Data:", typeof bankName);
+
+console.log("\n ***************** After Initialization **********************");
+var bankName = "SBI Bank";
+console.log("Bank Name:", bankName, "       Type Of Data:", typeof bankName);
+
+console.log("\n ***************** Task - 02 **********************");
+var marks = "90%";
+console.log("Marks:", marks, "       Type Of Data:", typeof marks);
+
+console.log("\n ***************** Task - 03  **********************");
+console.log(
+  "\n ***************** Before Initialization **********************"
+);
+var isWorking;
+console.log(
+  "IsWorking value:",
+  isWorking,
+  "       Type Of Data:",
+  typeof isWorking
+);
+
+console.log("\n ***************** After Initialization **********************");
+var isWorking = false;
+console.log(
+  "IsWorking value:",
+  isWorking,
+  "       Type Of Data:",
+  typeof isWorking
+);
+
+console.log("\n ***************** Task - 04 A **********************");
+
+var totalCount = "Hundred and Seven";
+console.log(
+  "TOTAL COUNT:",
+  totalCount,
+  "       Type Of Data:",
+  typeof totalCount
+);
+
+console.log("\n ***************** B **********************");
+var totalCount = 107;
+console.log(
+  "TOTAL COUNT:",
+  totalCount,
+  "       Type Of Data:",
+  typeof totalCount
+);
--- 02_variableBasicsAssign.js
+++ 02_variableBasicsAssign.js
@@ -1,12 +1,19 @@
 console.log("***************** ASSIGNMENT 01 **********************");
 
-console.log("\n***************** 3 Different Data types **********************");
+console.log(
+  "\n***************** 3 Different Data types **********************"
+);
 
 var numberVariable = 123;
 var numVariable = 100;
 var numVar = 200;
 
-console.log("\nNumberVariable :", numberVariable, "   type :", typeof numberVariable);
+console.log(
+  "\nNumberVariable :",
+  numberVariable,
+  "   type :",
+  typeof numberVariable
+);
 console.log("NumberVariable :", numVariable, "     type :", typeof numVariable);
 console.log("NumberVariable :", numVar, "         type :", typeof numVar);
 
@@ -16,8 +23,18 @@ var stringVariable = "Hello, world😊!";
 var strVariable = "Hello, India❤️!!";
 var strVar = "Hello, Pune😍!";
 
-console.log("\nStringVariable :", stringVariable, "    type of Data:", typeof stringVariable);
-console.log("StringVariable :", strVariable, "    type of Data:", typeof strVariable);
+console.log(
+  "\nStringVariable :",
+  stringVariable,
+  "    type of Data:",
+  typeof stringVariable
+);
+console.log(
+  "StringVariable :",
+  strVariable,
+  "    type of Data:",
+  typeof strVariable
+);
 console.log("StringVariable :", strVar, "      type of Data:", typeof strVar);
 
 console.log("\n***************** BOOLEAN **********************");
@@ -26,11 +43,28 @@ var isTrue = true;
 var isFalse = false;
 var isSunny = true;
 
-console.log("\nBooleanVariable :", isTrue, "     type of Data :", typeof isTrue);
-console.log("BooleanVariable :",isFalse, "       type of Data :", typeof isFalse);
-console.log("BooleanVariable :",isSunny, "        type of Data :", typeof isSunny);
+console.log(
+  "\nBooleanVariable :",
+  isTrue,
+  "     type of Data :",
+  typeof isTrue
+);
+console.log(
+  "BooleanVariable :",
+  isFalse,
+  "       type of Data :",
+  typeof isFalse
+);
+console.log(
+  "BooleanVariable :",
+  isSunny,
+  "        type of Data :",
+  typeof isSunny
+);
 
-console.log("\n***************** Difference Between Undefined and Null **********************");
+console.log(
+  "\n***************** Difference Between Undefined and Null **********************"
+);
 
 var myVariable = null;
 console.log("\nNULL:", myVariable, "    type of Data:", typeof myVariable);
