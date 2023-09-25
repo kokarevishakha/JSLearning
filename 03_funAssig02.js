f3adbd15649d891fedb1ac164bf9b395d0cf1958
kokarevishakha <vishakhakokare07@gmail.com>
2023-09-25 13:47:37 +0530

    Task completed for function and basic string
    Task completed for function and basic string

---
+++ 03_funAssig02.js
@@ -0,0 +1,14 @@
+
+console.log("\n**************************************** ASSIGNMENT 01 NORMAL FUNCTION  ************************************************");
+  console.log("\n");
+function bankDetails(bankName, accountNum, location, pinCode) {
+  console.log(
+    "Bank Name: " + bankName,
+    "    Account Number: " + accountNum,
+    "    Location: " + location,
+    "     Pin Code: " + pinCode
+  );
+}
+bankDetails("CITI Bank", "3456782345", "Pune", "431202");
+bankDetails("Axis Bank", "7856782345", "Mumbai", "441202");
+bankDetails("HDFC Bank", "8956782345", "Pune", "631202", "Open");
---
+++ stringBasicAssig01.js
@@ -0,0 +1,25 @@
+console.log(
+  "**************************************** ASSIGNMENT_03 STRING BASIC  ************************************************"
+);
+console.log(
+  "**************************************** TASK-01  ************************************************"
+);
+
+function stringBasics() {
+  console.log("my Dream Company is :  AMDOCS");
+}
+stringBasics();
+
+console.log("\n\n"); // Add extra space for separation
+
+console.log(
+  "**************************************** TASK-02  ************************************************"
+);
+
+function myHobbies(hobby1, hobby2, hobby3) {
+  console.log("Hobbies Are:", hobby1, hobby2, hobby3);
+
+  var totalCharacters = hobby1.length + hobby2.length + hobby3.length;
+  console.log("Total number of characters:", totalCharacters);
+}
+myHobbies("Cycling", "Traveling", "Cricket");
