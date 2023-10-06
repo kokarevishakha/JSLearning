0fdb6990b1416a89bd58bb207fdc25af3136910d
kokarevishakha <vishakhakokare07@gmail.com>
2023-10-06 15:27:11 +0530

    Completed Task for array using all method and for in loop
    Completed Task for array using all method and for in loop

---
+++ 08_Arrayassig02.js
@@ -0,0 +1,168 @@
+console.log(
+    `================================= * Assignment 02 For Array   * ======================`
+  );
+  const arrayNumbers = [20, 31, 40, 25, 23, 11, 29, 9, 60, 2, 11];
+  console.log(`Given the Array :=> ${arrayNumbers}`);
+  
+  const lengthOfArray = arrayNumbers.length;
+  console.log(`Length Of Array :- ${lengthOfArray}`);
+
+  console.log(
+    `-------------------------------Q.2--------------------------------------------`
+  );
+
+  console.log(`First Element :=> ${arrayNumbers[0]}`);
+  console.log(`Last Element :=> ${arrayNumbers[arrayNumbers.length-1]}`);
+
+  console.log(
+    `-------------------------------Q.3--------------------------------------------`
+  );  
+  console.log(`3rd Last Element  :=> ${arrayNumbers[arrayNumbers.length-3]}`);
+
+  console.log(
+    `-------------------------------Q.4--------------------------------------------`
+  );  
+  var arrayNumber = [20, 31, 40, 25, 23, 11, 29, 9, 60, 2, 11];
+  console.log(`Given array :=>${arrayNumber}`);
+  console.log(`Even Number :=>`);
+
+  var evenElements = "";
+  for (const element of arrayNumber) {
+    if (element % 2 === 0) {
+        evenElements += element + " ";
+      
+    }
+  }
+  console.log(evenElements.trim());
+
+  console.log(
+    `-------------------------------Q.5--------------------------------------------`
+  );  
+  var arrayNumber = [20, 31, 40, 25, 23, 11, 29, 9, 60, 2, 11];
+  console.log(`Given array :=>${arrayNumber}`);
+console.log(`Odd elements:`);
+
+var oddElements = "";
+for (const element of arrayNumber) {
+  if (element % 2 !== 0) { // Check if element is odd
+    oddElements += element + " ";
+  }
+}
+
+console.log(oddElements.trim()); // Trim to remove trailing space
+
+console.log(
+    `-------------------------------Q.6--------------------------------------------`
+  ); 
+  
+  var arrayNumber = [20, 31, 40, 25, 23, 11, 29, 9, 60, 2, 11];
+console.log(`Even elements:`);
+
+var evenElements = "";
+var evenSum = 0;
+
+for (const element of arrayNumber) {
+  if (element % 2 === 0) {
+    evenElements += element + " ";
+    evenSum += element;
+  }
+}
+
+console.log(evenElements.trim()); // Trim to remove trailing space
+console.log(`Sum of even elements: ${evenSum}`);
+
+console.log(
+    `-------------------------------Q.7--------------------------------------------`
+  ); 
+
+  var arrayNumber = [20, 31, 40, 25, 23, 11, 29, 9, 60, 2, 11];
+  console.log(`Given array :=>${arrayNumber}`);
+console.log(`Odd elements:`);
+
+var oddElements = "";
+var oddSum=0;
+for (const element of arrayNumber) {
+  if (element % 2 !== 0) { // Check if element is odd
+    oddElements += element + " ";
+    oddSum += element;
+  }
+}
+
+console.log(oddElements.trim()); 
+console.log(`Sum of Odd elements: ${oddSum}`);
+
+console.log(
+    `-------------------------------Q.8--------------------------------------------`
+  );
+  var arrayNumber = [20, 31, 40, 25, 23, 11, 29, 9, 60, 2, 11];
+console.log(`Numbers that are multiples of 5:`);
+
+var multiplesOf5 = "";
+for (const element of arrayNumber) {
+  if (element % 5 === 0) {
+    multiplesOf5 += element + " ";
+  }
+}
+
+console.log(multiplesOf5.trim()); 
+
+console.log(
+    `-------------------------------Q.9--------------------------------------------`
+  );
+  var arrayNumber = [20, 31, 40, 25, 23, 11, 29, 9, 60, 2, 11];
+console.log(`Sum Of All Element :`);
+var sum = 0;
+for (const element of arrayNumber) {
+  sum += element;
+}
+console.log(`Sum Of Element :=> ${sum}`);
+
+console.log(
+    `-------------------------------Q.10--------------------------------------------`
+  );
+  var arrayNumber = [20, 31, 40, 25, 23, 11, 29, 9, 60, 2, 11];
+var is115Present = false;
+
+for (const element of arrayNumber) {
+  is115Present = is115Present || element === 115;
+}
+
+console.log(is115Present ? "115 is present in the array." : "115 is not present in the array.");
+
+console.log(
+    `-------------------------------Q.11--------------------------------------------`
+  );
+  var arrayNumber = [20, 31, 40, 25, 23, 11, 29, 9, 60, 2, 11];
+var is23Present = false;
+
+for (const element of arrayNumber) {
+  is23Present = is23Present || element === 23;
+}
+
+console.log(is23Present ? "23 is present in the array." : "23 is not present in the array.");
+
+console.log(
+    `-------------------------------Q.12--------------------------------------------`
+  );
+  var arrayNumber = [20, 31, 40, 25, 23, 11, 29, 9, 60, 2, 11];
+  console.log(`Given Original array  :=>${arrayNumber}`);
+
+  arrayNumber.splice(3, 0, 55, 66);
+  console.log(`Insert Number :=> ${arrayNumber}`);
+
+  console.log(
+    `-------------------------------Q.13--------------------------------------------`
+  );
+
+  var arrayNumber = [20, 31, 40, 25, 23, 11, 29, 9, 60, 2, 11];
+console.log(`Original Array: ${arrayNumber}`);
+
+arrayNumber.splice(4, 3); // Starting from index 4, remove 3 elements
+
+console.log(`Array after deletion: ${arrayNumber}`);
+
+
+  
+
+
+  
---
+++ 08_arrayAssigA.js
@@ -0,0 +1,83 @@
+console.log(
+    `================================= * Assignment For Array   * ======================`
+  );
+  const arrayFruits = ["Banana", "Orange", "Apple", "Mango", "Water Melon"];
+    console.log(`Display Array Element :=> ${arrayFruits} `);
+    // Log the first element
+    console.log(`First Element:=> ${arrayFruits[0]}`);
+    
+    // Log the last element
+    console.log(`Last Element:=> ${arrayFruits[arrayFruits.length - 1]}`);
+
+    console.log(
+        `-------------------------------Q.2--------------------------------------------`
+      );
+      arrayFruits.unshift("Papaya");
+      console.log(`Add The Element "Papaya" Before Banana :=> ${arrayFruits}`);
+
+      console.log(
+        `-------------------------------Q.3--------------------------------------------`
+      );
+
+      var removedElements = arrayFruits.splice(4, 1);
+      console.log(`Remove "Mango" from the array :=> ${arrayFruits}`);
+
+      console.log(
+        `-------------------------------Q.4--------------------------------------------`
+      );
+      
+      let arrayFruit = ["Banana", "Orange", "Apple", "Mango", "Water Melon"];
+      arrayFruit.push("Pineapple"); // Add "Pineapple" to the end of the array
+      console.log(`Add Element or Insert an Element "Pineapple" at Last Position :=> ${arrayFruit}`);
+     
+      console.log(
+        `-------------------------------Q.5--------------------------------------------`
+      );
+      let arrayFruit1 = ["Banana", "Orange", "Apple", "Mango", "Water Melon"];
+      console.log(`Given The Array :=> ${arrayFruit1}`);
+      arrayFruit1.splice(4,0,"Dragon Fruit");
+      console.log(`Insert an Element :=> ${arrayFruit1}`);
+
+      console.log(
+        `-------------------------------Q.6--------------------------------------------`
+      );
+      let arrayFruit2 = ["Banana", "Orange", "Apple", "Mango", "Water Melon"];
+      console.log(`Given The Array :=> ${arrayFruit2}`);
+      
+      arrayFruit2.splice(1, 1, "Kiwi"); // Remove 1 element starting from index 1 and add "Kiwi" in its place
+      console.log(`Replace an Element :=> ${arrayFruit2}`);
+      
+      console.log(
+        `-------------------------------Q.7--------------------------------------------`
+      );
+      var arrayFruit3 = ["Banana", "Orange", "Apple", "Mango", "Water Melon"];
+      console.log(`Given The Array :=> ${arrayFruit3}`);
+      for(let i=0;i<=arrayFruit3.length-1;i++)
+      {
+        const element=arrayFruit3[i];
+       console.log(`Index: ${i}  value: ${element}`);
+      }
+
+      console.log(
+        `-------------------------------Q.8--------------------------------------------`
+      );
+      var arrayFruit3 = ["Banana", "Orange", "Apple", "Mango", "Water Melon"];
+      console.log(`Given The Array :=> ${arrayFruit3}`);
+     var lastThreeElements = arrayFruit3.slice(-3);
+
+     console.log(`Last three elements: ${lastThreeElements}`);
+
+    
+
+
+
+         
+     
+
+
+
+
+  
+  
+
+
